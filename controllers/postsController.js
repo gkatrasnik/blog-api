87b2aa1e3d1e644a33f2e23470bde78287eda8e9
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-11-09 14:53:02 +0100

    posts routes and controllers works

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -9,7 +9,7 @@ exports.list_GET = (req, res, next) => {
         return next(err);
       }
       //Successful, so send
-      res.json(list_posts);
+      res.status(200).json(list_posts);
     });
 };
 
@@ -22,7 +22,7 @@ exports.list_published_GET = (req, res, next) => {
         return next(err);
       }
       //Successful, so send
-      res.json(list_posts);
+      res.status(200).json(list_posts);
     });
 };
 
@@ -35,6 +35,92 @@ exports.list_unpublished_GET = (req, res, next) => {
         return next(err);
       }
       //Successful, so send
-      res.json(list_posts);
+      res.status(200).json(list_posts);
     });
+};
+
+//post new post
+exports.post_POST = (req, res, next) => {
+  const { userId, title, content } = req.body;
+  const newPost = new Post({
+    title: title,
+    user: userId,
+    content: content,
+    comments: [],
+    timestamp: Date.now(),
+    published: true,
+  });
+
+  newPost
+    .save()
+    .then((post) => {
+      res.status(200).json({ success: true, post });
+    })
+    .catch((err) => {
+      return next(err);
+    });
+};
+
+// get post
+exports.post_GET = (req, res, next) => {
+  Post.findById(req.params.id)
+    .populate("user")
+    .exec(function (err, post) {
+      if (err) {
+        return res.status(500).json({ success: false, msg: err.message });
+      }
+      if (post == null) {
+        const err = new Error("Not found");
+        return res.status(404).json({ success: false, msg: err.message });
+      }
+      return res.status(200).json({ success: true, post: post });
+    });
+};
+
+//delete post
+exports.post_DELETE = (req, res) => {
+  Post.findByIdAndDelete(req.params.id, (err, deletedPost) => {
+    if (err) {
+      return res.status(409).json({ success: false, msg: err.message });
+    } else {
+      res.status(200).json({
+        success: true,
+        msg: "Post deleted",
+        post: deletedPost,
+      });
+    }
+  });
+};
+
+//update post
+exports.post_PUT = (req, res) => {
+  const { userId, title, content, published } = req.body;
+  const updatedPost = new Post({
+    title: title,
+    user: userId,
+    content: content,
+    comments: [],
+    timestamp: Date.now(),
+    published: published,
+    _id: req.params.id,
+  });
+
+  Post.findByIdAndUpdate(
+    req.params.id,
+    updatedPost,
+    function (err, updatedpost) {
+      if (err) {
+        return res.status(400).json({ success: false, msg: err.message });
+      }
+      updatedpost.populate("user", (err, post) => {
+        if (err) return next(err);
+
+        return res.status(200).json({
+          success: true,
+          msg: "Successfuly updated post",
+          post: post,
+        });
+      });
+    }
+  );
 };
--- routes/posts.js
+++ routes/posts.js
@@ -4,12 +4,9 @@ const postsController = require("../controllers/postsController");
 const passport = require("passport");
 
 //Get lists of posts
-router.get(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  postsController.list_GET
-);
+router.get("/", postsController.list_GET);
 
+// protected
 router.get(
   "/published",
   passport.authenticate("jwt", { session: false }),
@@ -22,4 +19,29 @@ router.get(
   postsController.list_unpublished_GET
 );
 
+// get post
+router.get("/:id", postsController.post_GET);
+
+//protected
+
+// create new post
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  postsController.post_POST
+);
+
+// delete post
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  postsController.post_DELETE
+);
+
+// update post
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  postsController.post_PUT
+);
 module.exports = router;
